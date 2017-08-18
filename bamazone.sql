39920b494ecf456c5aef137579b29304419b14ad
Ruby Dease <rubydease@me.com>
2017-08-17 21:24:44 -0400

    update

--- bamazone.sql
+++ bamazone.sql
@@ -3,24 +3,27 @@ CREATE DATABASE bamazone_db;
 USE bamazone_db;
  
  CREATE TABLE products(
- item_id INT (11) NOT NULL,
- product_name VARCHAR (100),
- department_name VARCHAR (100),
- price VARCHAR(100),
- stock_quantity VARCHAR(100),
- PRIMARY KEY (item_id)
- );
+ itemID INT  NOT NULL,
+ productName VARCHAR (50) NOT NULL,
+ departmentName VARCHAR (50) NOT NULL,
+ price DECIMAL(6,2) NOT NULL,
+ StockQuantity INT NOT NULL
+);
+
  
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('1', 'shoes', 'womans', '120.00', '20');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('2', 'purse', 'womans', '90.00', '40');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('3', 'dress', 'womans', '150.00', '20');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('4', 'pants', 'womans', '85.00', '50');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('5', 'blouse', 'womans', '75.00', '40');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('6', 'skirt', 'womans', '95.00', '30');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('7', 'scarf', 'womans', '40.00', '30');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('8', 'wallets', 'womans', '60.00', '20');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('9', 'joga pants', 'womans', '80.00', '20');
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('10', 'rain coat', 'womans', '130.00', '20');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('1', 'shoes', 'womans', '120.00', '20');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('2', 'purse', 'womans', '90.00', '40');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('3', 'dress', 'womans', '150.00', '20');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('4', 'pants', 'womans', '85.00', '50');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('5', 'blouse', 'womans', '75.00', '40');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('6', 'skirt', 'womans', '95.00', '30');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('7', 'scarf', 'womans', '40.00', '30');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('8', 'wallets', 'womans', '60.00', '20');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('9', 'joga pants', 'womans', '80.00', '20');
+INSERT INTO products (itemID, productName, departmentName, price, stockQuantity) values ('10', 'rain coat', 'womans', '130.00', '20');
+ 
+ 	
+ 
  
  SELECT * FROM products;
  
